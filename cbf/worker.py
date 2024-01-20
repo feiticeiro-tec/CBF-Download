308c44564118022a513cfc58e7742e202a3bc6d4
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2024-01-20 01:04:53 +0000

    add compress and move script to session

--- cbf/worker.py
+++ cbf/worker.py
@@ -1,13 +1,17 @@
 from celery import Celery
 from . import CBF
-from config import RABBITMQ_BROKER
+from config import RABBITMQ_BROKER, RESULT_EXPIRES
 from loguru import logger
+import gzip
 
 app = Celery("cbf", broker=RABBITMQ_BROKER, backend="rpc://")
+app.conf.update(
+    result_expires=RESULT_EXPIRES,  # 1 hour
+)
 
 
 @app.task(name="cbf.download")
-def download(serie, ano, partida, reply_to):
+def download(serie, ano, partida):
     try:
         cbf = CBF()
         url = cbf.get_url_sumula_external(serie, ano, partida)
@@ -16,5 +20,19 @@ def download(serie, ano, partida, reply_to):
         logger.error("Error ao baixar arquivo.", exception=e)
         return
 
-    logger.success(f"File sent to {reply_to}")
-    return file
+    logger.success(
+        "Arquivo baixado com sucesso!",
+        params={
+            "serie": serie,
+            "ano": ano,
+            "partida": partida,
+        },
+    )
+
+    file = gzip.compress(file)
+    return {
+        "serie": serie,
+        "ano": ano,
+        "partida": partida,
+        "file": file.decode("latin-1"),
+    }
