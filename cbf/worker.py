c6e1fdd401345e9147fe9bc041d791e9a1587d26
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2024-01-20 01:38:33 +0000

    add service cbf.parse

--- cbf/worker.py
+++ cbf/worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from . import CBF
+from . import CBFSession, CBFExtrat
 from config import RABBITMQ_BROKER, RESULT_EXPIRES
 from loguru import logger
 import gzip
@@ -13,13 +13,14 @@ app.conf.update(
 @app.task(name="cbf.download")
 def download(serie, ano, partida):
     try:
-        cbf = CBF()
+        cbf = CBFSession()
         url = cbf.get_url_sumula_external(serie, ano, partida)
         file = cbf.request_download_file(url)
     except Exception as e:
         logger.error("Error ao baixar arquivo.", exception=e)
         return
 
+    file = gzip.compress(file)
     logger.success(
         "Arquivo baixado com sucesso!",
         params={
@@ -28,11 +29,32 @@ def download(serie, ano, partida):
             "partida": partida,
         },
     )
-
-    file = gzip.compress(file)
     return {
         "serie": serie,
         "ano": ano,
         "partida": partida,
         "file": file.decode("latin-1"),
     }
+
+
+@app.task(name="cbf.parse")
+def parse(file):
+    try:
+        file_encode = file.encode("latin-1")
+        file_bytes = gzip.decompress(file_encode)
+    except Exception as e:
+        logger.error("Error ao descomprimir arquivo.", exception=e)
+        return
+    try:
+        extract = CBFExtrat(False)
+        text = extract.bytes_pdf_to_text(file_bytes)
+    except Exception as e:
+        logger.error("Error ao converter arquivo.", exception=e)
+        return
+    try:
+        data = extract.get_data(text)
+        logger.success("Dados extraidos com sucesso!")
+        return data
+    except Exception as e:
+        logger.error("Error ao extrair dados.", exception=e)
+        return
