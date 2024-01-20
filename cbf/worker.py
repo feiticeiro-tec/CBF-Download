3a779551593e42c4a2ae1e69169a0a24827003a2
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2024-01-20 20:08:25 +0000

    add parse version 2 in worker

--- cbf/worker.py
+++ cbf/worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from . import CBFSession, CBFExtratV1
+from . import CBFSession, CBFExtratV1, CBFExtratV2
 from config import RABBITMQ_BROKER, RESULT_EXPIRES
 from loguru import logger
 import gzip
@@ -47,6 +47,29 @@ def parse(file):
         return
     try:
         extract = CBFExtratV1(False)
+        text = extract.bytes_pdf_to_text(file_bytes)
+    except Exception as e:
+        logger.error("Error ao converter arquivo.", exception=e)
+        return
+    try:
+        data = extract.get_data(text)
+        logger.success("Dados extraidos com sucesso!")
+        return data
+    except Exception as e:
+        logger.error("Error ao extrair dados.", exception=e)
+        return
+
+
+@app.task(name="cbf.parse.v2")
+def parse_v2(file):
+    try:
+        file_encode = file.encode("latin-1")
+        file_bytes = gzip.decompress(file_encode)
+    except Exception as e:
+        logger.error("Error ao descomprimir arquivo.", exception=e)
+        return
+    try:
+        extract = CBFExtratV2(False)
         text = extract.bytes_pdf_to_text(file_bytes)
     except Exception as e:
         logger.error("Error ao converter arquivo.", exception=e)
